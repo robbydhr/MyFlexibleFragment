daaa478735f0e5a681204c94ee9ecc8ba8bf7e74
robbydhr <robby.danu@gmail.com>
2019-11-28 21:55:15 +0700

    Final Apps

--- app/src/main/java/com/skripsi/robby/myflexiblefragment/HomeFragment.java
+++ app/src/main/java/com/skripsi/robby/myflexiblefragment/HomeFragment.java
@@ -4,6 +4,7 @@ package com.skripsi.robby.myflexiblefragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +36,16 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         btnCategory.setOnClickListener(this);
     }
     public void onClick(View v){
-
+        if (v.getId() == R.id.btn_category) {
+            CategoryFragment mCategoryFragment = new CategoryFragment();
+            FragmentManager mFragmentManager = getFragmentManager();
+            if (mFragmentManager != null) {
+                mFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, mCategoryFragment, CategoryFragment.class.getSimpleName())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        }
     }
 }
